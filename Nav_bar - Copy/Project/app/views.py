88e8415b94c0a244b27353b06a0f4c3edb51cb56
SheikhDanish22 <danish1521@gmail.com>
2025-01-12 11:21:36 +0530

    nav_copy

---
+++ Nav_bar - Copy/Project/app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+def landingpage(request):
+    return render(request,'landingpage.html')
+
+def home(request):
+    return render(request,'home.html')
+
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def ragister(request):
+    return render(request,'ragister.html')
